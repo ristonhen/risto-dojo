265928e11b4bfa93e1db8b1a06f69ecbc9815d47
ristonhen <ristonhen@gmail.com>
2023-01-19 17:59:12 +0700

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>AboutPage</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, voluptatum sequi aut suscipit quaerat magni earum eaque quasi, recusandae assumenda sunt similique. Qui quae vitae voluptatum, consequuntur sunt corporis veniam.</p>   
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, voluptatum sequi aut suscipit quaerat magni earum eaque quasi, recusandae assumenda sunt similique. Qui quae vitae voluptatum, consequuntur sunt corporis veniam.</p>   
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2{
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p{
+    margin: 20px 0;
+}
+router-link-exact-active {
+      color: #12b488;
+    }
+</style>
